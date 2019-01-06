85e3cef12164f2d341eb2aa42e3dd275322bc5b3
Sergey Slotin <me@sereja.me>
2019-01-06 21:36:11 +0300

    Changing folder name

--- publish.sh
+++ publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-jupyter-nbconvert `find content -name *.ipynb`
+jupyter-nbconvert `find html -name *.ipynb`
 
-for dir in $(find content -type d | tail +2)
+for dir in $(find html -type d | tail +2)
 do
-    cp content/custom.css $dir/custom.css
-    cp content/custom.js $dir/custom.js
+    cp html/custom.css $dir/custom.css
+    cp html/custom.js $dir/custom.js
 done
 
-rsync -r content root@tgeneration.ru:/var/www
+rsync -r html root@tgeneration.ru:/var/www
 git add *
 read -p "Commit description: " desc
 git commit -m "$desc"
